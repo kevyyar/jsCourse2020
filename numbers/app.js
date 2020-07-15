143552d64166feea11d1e21d8fbc1c177befda99
Kevin Barreto <kevyyar@gmail.com>
2020-07-15 14:54:16 -0500

    initial commit

---
+++ numbers/app.js
@@ -0,0 +1,22 @@
+// Math Object
+
+console.log(Math.PI);
+console.log(Math.floor(3.456));
+console.log(Math.round(4.87));
+console.log(Math.pow(3, 2));
+
+console.log(Math.random());
+
+// Generate Random # between 1-1
+let randomNum = Math.floor(Math.random() * 10) + 1;
+console.log(randomNum);
+
+// parseInt & parseFloat
+
+console.log(parseInt(24));
+console.log(parseInt(23.345));
+console.log(parseInt("29 days later"));
+
+console.log(parseFloat(2.348));
+console.log(parseFloat("$99.99"));
+console.log(parseFloat("99cents"));
---
+++ strings/app.js
@@ -0,0 +1,26 @@
+console.log("this is so cool");
+
+// String Indices
+let animal = "chicken";
+console.log(animal[3]);
+
+// show lenght of string
+let name = "Kevin";
+console.log(name.length);
+
+// show last character of any string or array
+let gibberish = "fhlsadhflsbdf743ig4jbjhkfbsajf)()$%#uf";
+console.log(gibberish[gibberish.length - 1]);
+
+//////////// String Methods
+let message = "you are so ground mister!";
+console.log(message.toUpperCase());
+
+let angry = "LeAvE mE aLoNe";
+console.log(angry.toLowerCase());
+
+let color = "  yellow";
+console.log(color.trim());
+
+let sport = "baseball";
+console.log(sport.indexOf("ball"));
