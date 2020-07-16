b6110ccf7a631f83e5073303bb21e15e06bfc8f6
Kevin Barreto <barretokevin@hotmail.com>
2020-07-15 20:19:34 -0500

    Added a ternary operator example

--- conditionals/app.js
+++ conditionals/app.js
@@ -2,12 +2,25 @@ let password = "hello there";
 
 // check length of passwd
 
-if (password.length >= 7) {
-  if (password.indexOf(" ") === -1) {
-    console.log("password is valid");
-  } else {
-    console.log("password is long enough but it cannot contain spaces");
-  }
-} else {
-  console.log("password is not valid");
-}
+// if (password.length >= 7) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("password is valid");
+//   } else {
+//     console.log("password is long enough but it cannot contain spaces");
+//   }
+// } else {
+//   console.log("password is not valid");
+// }
+
+let status = "offline";
+// let color;
+
+// if (status === "offline") {
+//   color = "red";
+// } else {
+//   color = "green";
+// }
+
+// Ternary operator
+let color = status === "offline" ? "red" : "green";
+console.log(color);
